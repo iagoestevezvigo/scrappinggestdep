af535b074dd933bf487f96e18277e30ad3c16da0
CriticizedCode <he.criticized@gmail.com>
2025-01-05 22:51:46 +0100

    solved table and also added the match results and the next matches

--- main.ts
+++ main.ts
@@ -1,62 +1,15 @@
 import * as fs from 'fs';
-import * as cheerio from 'cheerio';
-
-function getclasificationfromhtml(htmlContent: string): string[][] {
-    function ntype(n:number, i:number) {
-        var t = ["2", "5", "9", "4", "1", "0", "8", "6", "3", "7", "1", "3", "5", "7", "9", "0", "2", "4", "6", "8", "0", "2", "4", "6", "8", "1", "3", "5", "7", "9", "7", "5", "2", "0", "9", "6", "3", "8", "4", "1"];
-        const index = (i * 10) + n;  // Calc the value from n and i
-        const result = t[index];  // Get the value of the array in that specific position
-        return result
-    }
-    const $ = cheerio.load(htmlContent);
-
-    //Select body of the table with the clasification
-    const tbody = $('#CL_Resumen > div:nth-of-type(2) > table:nth-of-type(1) > tbody');
-        
-    const table: string[][] = [];
-    // Iterate through every row of the table
-    tbody.find('tr').each((rowIndex, row) => {
-        const rowData: string[] = [];
-        // Iterate through the columns of each row
-        $(row).find('td').each((colIndex, col) => {
-            if (colIndex>=3){
-                var value:string="";
-                const input = $(col).text()
-                // First, replace "ntype" with ";ntype" so we can split by ";"
-                const formattedString = input.replace(/ntype/g, ';ntype');
-
-                // Now split by ";"
-                const result = formattedString.split(';').filter(item => item.trim() !== '');
-                // console.log(input)
-                // console.log(result);
-                result.forEach(item => {
-                    if (item.includes('ntype')) {
-                        const values = item.split(',');
-
-                        const n = parseInt(values[1].trim());
-                        const i = parseInt(values[2].trim());
-                        value=value+ntype(n,i);
-                    }
-                    else{
-                        value=value+item
-                    }
-                });
-                // console.log(value);
-                // console.log("---------")
-                rowData.push(value)
-            }
-        });
-        if (rowData.length>0){
-            table.push(rowData); // Adding the row to the clasification table
-        }
-    });
-    
-    return table;
-}
+import * as utils from './src/utils';
 
 
 const htmlContent = fs.readFileSync('file.html', 'utf-8');
 
-const table = getclasificationfromhtml(htmlContent);
+var table = utils.getclasificationfromhtml(htmlContent);
+console.log(table); // Show the table
+
+table = utils.getlastmatchresults(htmlContent);
+console.log(table); // Show the table
+
+table = utils.getnextmatchs(htmlContent);
 console.log(table); // Show the table
   
---
+++ src/utils.ts
@@ -0,0 +1,70 @@
+import * as cheerio from 'cheerio';
+export function getclasificationfromhtml(htmlContent: string): string[][] {
+    const $ = cheerio.load(htmlContent);
+
+    // Select the element with the ID 'tableClasif'
+    const tableClasif = $('#tableClasif tbody');
+        
+    const table: string[][] = [];
+
+    // Iterate through all rows (<tr>) inside the tbody
+    tableClasif.find('tr').each((rowindex, row) => {
+        if(rowindex>=1){
+            const rowData: string[] = [];
+            
+            // For each row, find all cells (<td>) and extract their text
+            $(row).find('td').each((colindex, cell) => {
+                if (colindex!=1){
+                    rowData.push($(cell).text().trim());
+                }
+                else{
+                    const img = $(cell).find('img');
+                    rowData.push( img.attr('src') ? ("https://www.lapreferente.com/"+img.attr('src')):'No image source');
+                }
+            });
+        
+            // Log the extracted row data
+            table.push(rowData)
+        }
+        
+    });
+    return table;
+}
+export function getresultsfromtable(htmlContent: string, position_of_table:number): string[][]{
+    const $ = cheerio.load(htmlContent);
+
+    // Select the element with the ID 'tableClasif'
+    const tableClasif = $('.lpfTable01 tbody').eq(position_of_table);
+        
+    const table: string[][] = [];
+
+    // Iterate through all rows (<tr>) inside the tbody
+    tableClasif.find('tr').each((rowindex, row) => {
+        if(rowindex>=1){
+            const rowData: string[] = [];
+            
+            // For each row, find all cells (<td>) and extract their text
+            $(row).find('td').each((colindex, cell) => {
+                if (colindex!=1){
+                    rowData.push($(cell).text().trim());
+                }
+                else{
+                    rowData.push($(cell).text().trim());
+                }
+            });
+        
+            // Add data to the table
+            table.push(rowData)
+        }
+        
+    });
+    return table
+}
+
+export function getlastmatchresults(htmlContent: string): string[][] {
+    return getresultsfromtable(htmlContent,0);
+}
+
+export function getnextmatchs(htmlContent: string): string[][] {
+    return getresultsfromtable(htmlContent,2);
+}
