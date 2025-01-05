f0b29aa38a552e16d721bdbedc40e7d4b5d5d238
CriticizedCode <he.criticized@gmail.com>
2025-01-05 16:29:05 +0100

    Translated comments to english, cleared the code a little bit and added gitignore

--- main.ts
+++ main.ts
@@ -2,9 +2,6 @@ import * as fs from 'fs';
 import * as cheerio from 'cheerio';
 
 const htmlContent = fs.readFileSync('file.html', 'utf-8');
-const anExampleVariable = "Hello World"
-console.log(anExampleVariable)
-console.log(htmlContent)
 const $ = cheerio.load(htmlContent);
 
 // Scrape some content
@@ -12,28 +9,20 @@ const heading = $('h1').text();
 const paragraph = $('p').text();
 
 
-// Puedes manipular o imprimir la tabla aquí
-// Seleccionar solo la primera tabla con las clases 'table' y 'table-bordered'
-// const table = $('table.table.table-bordered').first();
-// // Buscar el <tbody> dentro de cada tabla
-// const tbody = $(table).find('tbody');
-
+//Select body of the table with the clasification
 const tbody = $('#CL_Resumen > div:nth-of-type(2) > table:nth-of-type(1) > tbody');
     
-// Iterar sobre las filas (<tr>) dentro del <tbody>
+// Iterate through every row of the table
 tbody.find('tr').each((rowIndex, row) => {
     const rowData: string[] = [];  // Definir el tipo explícito del array como string[]
     
-    // Iterar sobre las celdas (<td>) dentro de cada fila
+    // Iterate through the columns of each row
     $(row).find('td').each((colIndex, col) => {
         if (colIndex >= 3){
-            rowData.push($(col).text().replace(/\s+/g, ' ').trim()); // Obtener el texto de cada celda
+            rowData.push($(col).text().replace(/\s+/g, ' ').trim()); // Adding the text of every cel to a string list
         }
     });
     
-    console.log(rowData.join('|')); // Mostrar los datos de la fila
+    console.log(rowData.join('|')); // Show the data of the row
 });
-
-// To learn more about the language, click above in "Examples" or "What's New".
-// Otherwise, get started by removing these comments and the world is your playground.
   
